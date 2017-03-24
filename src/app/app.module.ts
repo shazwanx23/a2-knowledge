0ec0d6984123a8da12c88e7d65f5f7976741ab66
Nor Shazwan Shah Nor Hisham Shah <shazwan.hisham@mimos.local>
2017-03-24 10:09:07 +0800

    Added routing with three components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { HomeComponent }  from './components/home/home.component';
+import { ListComponent }  from './components/list/list.component';
+import { ViewComponent }  from './components/view/view.component';
+
+const routes: Routes = [
+	{
+		path: '', component: HomeComponent
+	},
+	{
+		path: 'list', component: ListComponent
+	},
+	{
+		path: 'view', component: ViewComponent
+	}
+]
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,
+  				  RouterModule.forRoot(routes)
+  				   ],
+  declarations: [ AppComponent,
+  				  HomeComponent,
+  				  ListComponent,
+  				  ViewComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'list',
+	templateUrl: 'list.component.html'
+})
+export class ListComponent{
+	
+}
