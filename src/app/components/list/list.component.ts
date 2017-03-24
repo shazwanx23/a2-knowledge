b15bf42575f02ae7b24b116d5347b2602d09395e
Nor Shazwan Shah Nor Hisham Shah <shazwan.hisham@mimos.local>
2017-03-24 10:45:11 +0800

    added ngfor

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Person{
+	firstName: string;
+	lastname: string;
+	username: string;
+}
+
 @Component({
 	moduleId: module.id,
 	selector: 'list',
 	templateUrl: 'list.component.html'
 })
 export class ListComponent{
-	
+	persons : Person[];
+
+	constructor() {
+		this.persons = [
+			{firstName: 'shazwan', lastname:  'shah', username: 'shazwanshah'},
+			{firstName: 'shazwan1', lastname:  'shah1', username: 'shazwanshah1'},
+			{firstName: 'shazwan2', lastname:  'shah2', username: 'shazwanshah2'},			
+		];
+	}
 }
